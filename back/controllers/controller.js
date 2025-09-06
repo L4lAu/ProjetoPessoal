2f6fd403aae44479daf64627db052cd35258d20f
L4lAu <duduzinho172007@gmail.com>
2025-09-05 22:27:26 -0300

    feat: adicionar criarContatoController [WIP]

--- back/controllers/controller.js
+++ back/controllers/controller.js
@@ -1,12 +1,12 @@
-import  {listarUsuarios, obterUsuarioPorId, cadastrarUsuario, criarContato, atualizarContato, excluiContato}  from "../models/model.js";
+import { listarUsuarios, obterUsuarioPorId, cadastrarUsuario, criarContato, atualizarContato, excluiContato } from "../models/model.js";
 
 
 const listarUsuariosController = async (req, res) => {
-    try{
+    try {
         const usuarios = await listarUsuarios();
         res.status(200).send(usuarios);
     } catch (err) {
-        res.status(500).json({message: 'erro interno no servidor', err});
+        res.status(500).json({ message: 'erro interno no servidor', err });
         console.error(err);
     };
 };
@@ -15,20 +15,20 @@ const obterUsuarioPorIdController = async (req, res) => {
     try {
         const usuario = await obterUsuarioPorId(req.params.id);
 
-        if(!usuario){
-            return res.status(404).json({message: 'usuario não encontrado' });
+        if (!usuario) {
+            return res.status(404).json({ message: 'usuario não encontrado' });
         }
 
         res.status(200).json(usuario);
     } catch (err) {
-        res.status(500).json({message: "erro interno no servidor: ", err});
+        res.status(500).json({ message: "erro interno no servidor: ", err });
         console.error(err);
     };
 };
 
 const cadastrarUsuarioController = async (req, res) => {
-    try{
-        const {nome, email, senha, tipo} = req.body;
+    try {
+        const { nome, email, senha, tipo } = req.body;
 
         const usuarioData = {
             nome: nome,
@@ -37,9 +37,15 @@ const cadastrarUsuarioController = async (req, res) => {
         };
 
         const usuarioNovo = await cadastrarUsuario(usuarioData);
-        res.status(201).json({message: "usuario criado com sucesso", usuarioNovo});
+        res.status(201).json({ message: "usuario criado com sucesso", usuarioNovo });
     } catch (err) {
         res.status(500).json("erro ao cadastrar usuario", err)
         console.error('erro ao cadastrar usuario', err);
     };
 };
+
+const criarContatoController = async (req, res) => {
+    try {
+        const {apelido, usuario_id}
+    }
+}
