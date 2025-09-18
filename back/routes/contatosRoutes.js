0de1f7aa0f2d66c4e2e925e36e03e73bf6886bbb
L4lAu <duduzinho172007@gmail.com>
2025-09-18 19:07:29 -0300

    feat: adicionar rota para criar contato

---
+++ back/routes/contatosRoutes.js
@@ -0,0 +1,6 @@
+import { criarContatoController, atualizarIdContatoController, atualizarApelidoContatoController, excluiContatoController } from "../controllers/contatosController.js";
+import express from "express";
+
+const router = express.Router();
+
+router.get("/criarContato", criarContatoController);
